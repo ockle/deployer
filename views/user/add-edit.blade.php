cded93f515ba18422a79d47430990b8350c578f6
ockle <ockle@users.noreply.github.com>
2014-01-03 23:29:33 +0000

    changed add/edit user page to use tabs

--- views/user/add-edit.blade.php
+++ views/user/add-edit.blade.php
@@ -9,40 +9,48 @@
 
 @include('partial.success-error')
 
+<dl class="tabs" data-tab>
+	<dd class="active">
+		<a href="#details">Details</a>
+	</dd>
+	<dd>
+		<a href="#host-accounts">Host accounts</a>
+	</dd>
+</dl>
+
+
 <form action="" method="post">
-	<fieldset>
-		<legend>Details</legend>
+	<div class="tabs-content">
+		<div class="content active column" id="details">
+			<label>First name</label>
+			<input type="text" name="first_name" value="{{{ $app->oldValue('first_name') ?: (isset($user->first_name) ? $user->first_name : '') }}}">
 
-		<label>First name</label>
-		<input type="text" name="first_name" value="{{{ $app->oldValue('first_name') ?: (isset($user->first_name) ? $user->first_name : '') }}}">
+			<label>Last name</label>
+			<input type="text" name="last_name" value="{{{ $app->oldValue('last_name') ?: (isset($user->last_name) ? $user->last_name : '') }}}">
 
-		<label>Last name</label>
-		<input type="text" name="last_name" value="{{{ $app->oldValue('last_name') ?: (isset($user->last_name) ? $user->last_name : '') }}}">
+			<label>Email address</label>
+			<input type="text" name="email" value="{{{ $app->oldValue('email') ?: (isset($user->email) ? $user->email : '') }}}">
 
-		<label>Email address</label>
-		<input type="text" name="email" value="{{{ $app->oldValue('email') ?: (isset($user->email) ? $user->email : '') }}}">
+			<label>Password</label>
+			<input type="password" name="password">
 
-		<label>Password</label>
-		<input type="password" name="password">
-
-		<label>Confirm password</label>
-		<input type="password" name="password_confirmation">
-	</fieldset>
-
-	<fieldset>
-		<legend>Host accounts</legend>
-
-		@if (!$hosts->isEmpty())
-			@foreach ($hosts as $host)
-			<label>{{{ $host->name }}}</label>
-			<input type="text" name="hosts[{{ $host->id }}]" value="{{{ (isset($user->hosts) && $user->hosts->contains($host->id)) ? $user->hosts->find($host->id)->pivot->username : '' }}}">
-			@endforeach
-		@else
-		<div class="information panel radius">
-			<p>There are currently no hosts</p>
+			<label>Confirm password</label>
+			<input type="password" name="password_confirmation">
 		</div>
-		@endif
-	</fieldset>
+
+		<div class="content column" id="host-accounts">
+			@if (!$hosts->isEmpty())
+				@foreach ($hosts as $host)
+				<label>{{{ $host->name }}}</label>
+				<input type="text" name="hosts[{{ $host->id }}]" value="{{{ (isset($user->hosts) && $user->hosts->contains($host->id)) ? $user->hosts->find($host->id)->pivot->username : '' }}}">
+				@endforeach
+			@else
+			<div class="information panel radius">
+				<p>There are currently no hosts</p>
+			</div>
+			@endif
+		</div>
+	</div>
 
 	<input type="hidden" name="type" value="{{ $type }}">
 
