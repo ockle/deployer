f5ecc9f70131cbb17e1eda22302189bb16a5e91e
ockle <ockle@users.noreply.github.com>
2014-02-09 22:24:14 +0000

    home page activity feed

--- classes/Controller/HomeController.php
+++ classes/Controller/HomeController.php
@@ -3,12 +3,16 @@
 namespace Deployer\Controller;
 
 use Deployer\Application;
+use Deployer\Model\Deployment;
 
 class HomeController
 {
     public function actionView(Application $app)
     {
+        $deployments = Deployment::with(array('project', 'user'))->mostRecentFirst()->get();
+
         $data = array(
+            'deployments' => $deployments
         );
 
         return $app['blade']->make('home', $data);
--- views/home.blade.php
+++ views/home.blade.php
@@ -5,27 +5,26 @@ Home
 @stop
 
 @section('content')
-<h2>Activity Feed</h2>
+<h2>Activity feed</h2>
 
+@if (!$deployments->isEmpty())
 <ul class="feed no-bullet">
+	@foreach ($deployments as $deployment)
 	<li class="row">
-		<div class="small-2 medium-1 columns">
-			<i class="fa fa-cogs round"></i>
+		<div class="small-2 medium-1 columns" title="{{ ucfirst($deployment->trigger) }} deployment">
+			<i class="fa {{ ($deployment->trigger == 'manual') ? 'fa-wrench' : 'fa-cogs' }} round"></i>
 		</div>
 		<div class="small-7 medium-9 columns">
-			<a href="">User</a> automatically deployed <a href="">Example.com</a>
+			<a href="{{ $app->path('user.edit', array('user' => $deployment->user->id)) }}">{{{ (isset($deployment->user)) ? $deployment->user->first_name : 'Unknown' }}} {{{ (isset($deployment->user)) ? $deployment->user->last_name : 'User' }}}</a> deployed <a href="{{ $app->path('project.view', array('project' => $deployment->project->id)) }}">{{{ $deployment->project->name }}}</a>
 		</div>
-		<div class="small-3 medium-2 columns"><span class="has-tip" title="01/01/2013 11:11:12" data-tooltip>1 hour ago</span></div>
+		<div class="small-3 medium-2 columns">
+			<span class="has-tip" title="{{ $deployment->created_at->format('d/m/Y H:i:s') }}" data-tooltip>{{ $deployment->created_at->diffForHumans() }}</span>
+		</div>
 	</li>
-
-	<li class="row">
-		<div class="small-2 medium-1 columns">
-			<i class="fa fa-wrench round"></i>
-		</div>
-		<div class="small-7 medium-9 columns">
-			<a href="">User</a> manually deployed <a href="">Example.com</a>
-		</div>
-		<div class="small-3 medium-2 columns"><span class="has-tip" title="01/01/2013 11:11:12" data-tooltip>1 hour ago</span></div>
-	</li>
+	@endforeach
 </ul>
+@else
+<div class="alert-box info radius">There have not yet been any deployments</div>
+@endif
+
 @stop
