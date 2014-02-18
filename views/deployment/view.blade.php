1522d30c6ca76c05cd8cc5fdf29080908131cc8d
ockle <ockle@users.noreply.github.com>
2014-02-18 23:46:43 +0000

    work on view deployment

--- views/deployment/view.blade.php
+++ views/deployment/view.blade.php
@@ -1,14 +1,31 @@
 @extends('layout')
 
 @section('title')
-
+Deployment details
 @stop
 
 @section('content')
-<h2>Deployment</h2>
+<h2>Deployment details</h2>
 
 @include('partial.success-error')
+<div class="row">
+		@if ($deployment->status)
+		<span class="label deployment-status success radius column">Success</span>
+		@else
+		<span class="label deployment-status alert radius column">Failure</span>
+		@endif
+</div>
+<dl class="panel radius">
+	<dt>Project</dt>
+	<dd>{{{ $deployment->project->name }}}</dd>
 
+	<dt>Deployed</dt>
+	<dd>{{ ($deployment->trigger == 'manual') ? 'Manually' : 'Automatically' }} at {{ $deployment->created_at->format('d/m/Y H:i:s') }} by <a href="{{ $app->path('user.edit', array('user' => $deployment->user->id)) }}">{{{ $deployment->user->first_name}}} {{{ $deployment->user->last_name }}}</a></dd>
 
+	<dt>Duration</dt>
+	<dd>{{ $deployment->duration }} ms</dd>
 
+	<dt>Log</dt>
+	<dd>{{ nl2br(e($deployment->log)) }}</dd>
+</dl>
 @stop
